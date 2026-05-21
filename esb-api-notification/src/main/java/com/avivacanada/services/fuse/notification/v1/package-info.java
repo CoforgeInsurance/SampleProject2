893fbc86e0dec6055d9796f9737519bc5d038b87
TanmaySaxenaCf <tanmay.saxena@coforge.com>
2026-05-21 16:02:36 +0530

    Delete esb-api-notification directory

--- esb-api-notification/src/main/java/com/avivacanada/services/fuse/notification/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.avivacanada.com/services/fuse/notification/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.avivacanada.services.fuse.notification.v1;
--- esb-api-notification/src/main/java/com/avivacanada/services/fuse/notificationcallback/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.avivacanada.com/services/fuse/notificationcallback/v1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.avivacanada.services.fuse.notificationcallback.v1;
